158c094f6ad9f64d3d7a444b241b84217a433ad9
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.co>
2023-11-08 00:41:04 +0530

    Updated fileupload functionality

--- index.js
+++ index.js
@@ -354,7 +354,6 @@ io.on("connection", (socket) => {
 
 server.listen(process.env.PORT, connectToMongoDB(), () => {
   console.log("app running faster");
-  console.log(process.env);
 });
 
 app.post("/live", authorizeToken, async (req, res) => {
@@ -516,6 +515,8 @@ async function authorize() {
 const uploadDirectory = "files/";
 
 // Function to upload a file to Google Drive
+const { Readable } = require("stream"); // Import the Readable class
+
 async function uploadFile(authClient, fileInfo) {
   return new Promise((resolve, reject) => {
     const drive = google.drive({ version: "v3", auth: authClient });
@@ -525,12 +526,17 @@ async function uploadFile(authClient, fileInfo) {
       parents: ["1CBsb1iOv_zEVn3A8JdxiiH3nWOrcUXpI"],
     };
 
+    // Create a readable stream from the memory buffer
+    const fileStream = new Readable();
+    fileStream.push(fileInfo.buffer);
+    fileStream.push(null); // Mark the end of the stream
+
     drive.files.create(
       {
         resource: fileMetaData,
         media: {
           mimeType: fileInfo.mimetype,
-          body: fs.createReadStream(`${uploadDirectory + fileInfo.filename}`),
+          body: fileStream,
         },
         fields: "id",
       },
@@ -547,22 +553,21 @@ async function uploadFile(authClient, fileInfo) {
 // Configure Multer to specify where to store uploaded files and their names.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    if (!fs.existsSync(uploadDirectory)) {
-      fs.mkdirSync(uploadDirectory);
-    }
-    cb(null, uploadDirectory); // Specify the directory where files will be stored.
+    cb(null, ''); // Specify the directory where files will be stored.
   },
   filename: function (req, file, cb) {
-    const timestamp = Date.now();
-    const originalname = file.originalname;
-    const ext = originalname.slice(((originalname.lastIndexOf(".") - 1) >>> 0) + 2);
-    const newFileName = `${timestamp}.${ext}`;
-    cb(null, newFileName);
+    // Use the current timestamp as a unique file name.
+    cb(null, Date.now() + file.originalname);
   },
 });
 
 // Configure multer to specify where to store uploaded files
-const upload = multer({ storage });
+const upload = multer({ storage: multer.memoryStorage({
+  filename: function (req, file, cb) {
+    // Use the current timestamp as a unique file name.
+    cb(null, Date.now() + file.originalname);
+  },
+}) });
 
 // Route to handle file upload
 app.post("/upload", upload.single("file"), authorizeToken, async (req, res) => {
@@ -572,6 +577,7 @@ app.post("/upload", upload.single("file"), authorizeToken, async (req, res) => {
 
   try {
     const authClient = await authorize(); // Implement your authorization logic here
+  // Access the uploaded file's binary data as a buffer
     const uploadedFile = await uploadFile(authClient, req.file);
     const fileId = uploadedFile.data.id;
     const filePath =
